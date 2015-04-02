ae035efa5ed97e2a2ec8749db2c278b88b30eee2
Ahmad Ben Mrad <batikhsouri@gmail.com>
2015-04-02 02:18:36 +0200

    Targets (and output) by platform

--- index.js
+++ index.js
@@ -3,94 +3,96 @@ var path = require('path');
 var im = require('imagemagick');
 var mkdirp = require('mkdirp');
 
-var targets = [
-	{
-		name: 'iTunesArtwork',
-		height: 512,
-		width: 512,
-		radius: 80
-	},
-	{
-		name: 'iTunesArtwork Retina',
-		height: 1024,
-		width: 1024,
-		radius: 160
-	},
-	{
-		name: 'iPhone',
-		height: 60,
-		width: 60,
-		radius: 9
-	},
-	{
-		name: 'iPhone@2x',
-		height: 120,
-		width: 120,
-		radius: 19
-	},
-	{
-		name: 'iPhone@3x',
-		height: 180,
-		width: 180,
-		radius: 28
-	},
-	{
-		name: 'iPad',
-		height: 76,
-		width: 76,
-		radius: 12
-	},
-	{
-		name: 'iPad@2x',
-		height: 152,
-		width: 152,
-		radius: 24
-	},
-	{
-		name: 'Google Play',
-		height: 512,
-		width: 512,
-		radius: 80
-	},
-	{
-		name: 'Android-ldpi',
-		height: 36,
-		width: 36,
-		radius: 6
-	},
-	{
-		name: 'Android-mdpi',
-		height: 48,
-		width: 48,
-		radius: 8
-	},
-	{
-		name: 'Android-hdpi',
-		height: 72,
-		width: 72,
-		radius: 11
-	},
-	{
-		name: 'Android-xhdpi',
-		height: 96,
-		width: 96,
-		radius: 15
-	},
-	{
-		name: 'Android-xxhdpi',
-		height: 144,
-		width: 144,
-		radius: 23
-	},
-	{
-		name: 'Android-xxxhdpi',
-		height: 192,
-		width: 192,
-		radius: 30
-	}
-];
-
-var iTarget = 0;
+var targets = {
+	"ios": [
+		{
+			name: 'iTunesArtwork',
+			height: 512,
+			width: 512,
+			radius: 80
+		},
+		{
+			name: 'iTunesArtwork Retina',
+			height: 1024,
+			width: 1024,
+			radius: 160
+		},
+		{
+			name: 'iPhone',
+			height: 60,
+			width: 60,
+			radius: 9
+		},
+		{
+			name: 'iPhone@2x',
+			height: 120,
+			width: 120,
+			radius: 19
+		},
+		{
+			name: 'iPhone@3x',
+			height: 180,
+			width: 180,
+			radius: 28
+		},
+		{
+			name: 'iPad',
+			height: 76,
+			width: 76,
+			radius: 12
+		},
+		{
+			name: 'iPad@2x',
+			height: 152,
+			width: 152,
+			radius: 24
+		}
+	],
+	"android": [
+		{
+			name: 'Google Play',
+			height: 512,
+			width: 512,
+			radius: 80
+		},
+		{
+			name: 'Android-ldpi',
+			height: 36,
+			width: 36,
+			radius: 6
+		},
+		{
+			name: 'Android-mdpi',
+			height: 48,
+			width: 48,
+			radius: 8
+		},
+		{
+			name: 'Android-hdpi',
+			height: 72,
+			width: 72,
+			radius: 11
+		},
+		{
+			name: 'Android-xhdpi',
+			height: 96,
+			width: 96,
+			radius: 15
+		},
+		{
+			name: 'Android-xxhdpi',
+			height: 144,
+			width: 144,
+			radius: 23
+		},
+		{
+			name: 'Android-xxxhdpi',
+			height: 192,
+			width: 192,
+			radius: 30
+		}
+	]
+};
 
 var baseIconPath = process.argv.length > 2 && process.argv[2];
 if (!baseIconPath){
@@ -121,50 +123,89 @@ if (!fs.existsSync(outputFolder)){
 	mkdirp(outputFolder, function(err){
 		if (err){
 			console.error('Error while creating folder ' + outputFolder);
+			console.error(err);
 			process.exit(1);
 		}
-		processOne();
+		conversion();
 	});
-} else processOne();
+} else conversion();
 
-function processOne(){
-	var currentTarget = targets[iTarget];
-	//var baseImage = gm(baseIconPath);
-	var destination = path.join(outputFolder, currentTarget.name + '.png');
-	var maskDest = path.join(outputFolder, 'mask.png');
-	console.log('Converting for ' + currentTarget.name);
-	im.convert([
-		'-size', currentTarget.width + 'x' + currentTarget.height,
-		'xc:none',
-		'-draw',
-		'roundrectangle 0,0,' + currentTarget.width + ',' + currentTarget.height + ',' + currentTarget.radius + ',' + currentTarget.radius,
-		maskDest
-	], function(err){
-		if (err){
-			console.error('Error with ImageMagick: ' + err);
-			process.exit(1);
+function conversion(){
+	var iTarget = 0;
+	var iPlatform = 0;
+	var currentPlatform;
+	var currentTargets;
+	var currentFolder;
+	var platforms = Object.keys(targets);
+
+	function processPlatform(){
+		currentPlatform = platforms[iPlatform];
+		console.log('Current platform: ' + currentPlatform);
+		currentTargets = targets[currentPlatform];
+		currentFolder = path.join(outputFolder, currentPlatform);
+		iTarget = 0;
+
+		mkdirp(currentFolder, function(err){
+			if (err){
+				console.error('Error while creating folder for platform ' + currentPlatform);
+				console.error(err);
+				process.exit(1);
+			}
+			processOne(platformEnd);
+		});
+
+		function platformEnd(){
+			iPlatform++;
+			if (iPlatform == platforms.length){
+				console.log('Done!');
+			} else {
+				processPlatform();
+			}
 		}
+	}
 
+	function processOne(end){
+		var currentTarget = currentTargets[iTarget];
+		//var baseImage = gm(baseIconPath);
+		var destination = path.join(currentFolder, currentTarget.name + '.png');
+		var maskDest = path.join(outputFolder, 'mask.png');
+		console.log('Converting for ' + currentTarget.name);
 		im.convert([
-			baseIconPath,
-			'-resize', currentTarget.width + 'x' + currentTarget.height,
-			'-matte',
-			maskDest,
-			'-compose',
-			'DstIn',
-			'-composite', destination
+			'-size', currentTarget.width + 'x' + currentTarget.height,
+			'xc:none',
+			'-draw',
+			'roundrectangle 0,0,' + currentTarget.width + ',' + currentTarget.height + ',' + currentTarget.radius + ',' + currentTarget.radius,
+			maskDest
 		], function(err){
 			if (err){
 				console.error('Error with ImageMagick: ' + err);
 				process.exit(1);
 			}
 
-			iTarget++;
-			if (iTarget == targets.length){
-				console.log('Done');
-			} else {
-				processOne();
-			}
+			im.convert([
+				baseIconPath,
+				'-resize', currentTarget.width + 'x' + currentTarget.height,
+				'-matte',
+				maskDest,
+				'-compose',
+				'DstIn',
+				'-composite', destination
+			], function(err){
+				if (err){
+					console.error('Error with ImageMagick: ' + err);
+					process.exit(1);
+				}
+
+				iTarget++;
+				if (iTarget == currentTargets.length){
+					end()
+				} else {
+					processOne(end);
+				}
+			});
 		});
-	});
+	}
+
+	processPlatform();
+
 }
